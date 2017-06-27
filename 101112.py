c9424bb3a3ee8d575c41ed25eb37cc2b2b8d5481
consultadd <mi4795jo@gmail.com>
2017-06-27 15:30:29 +0530

    message

---
+++ 101112.py
@@ -0,0 +1,40 @@
+#ex-10
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
+#ex-11
+
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (
+    22, 5'5, 53)
+
+
+#ex-12
+
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weigh? ")
+
+print "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
+
---
+++ 123.py
@@ -0,0 +1,29 @@
+print"hellooo!"
+print"this is mitch"
+print"typing is boring"
+print"i hate this"
+print"bla bla bla"
+print"don't do this!"
+print"this is fun" # no it's not."
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30 / 6
+
+# ex-3
+print "I will now count my chickens:"
+print "Hens", 25 + 60 / 6
+print "Is it true that 3 + 2 < 5 - 7?"
+print 3 + 2 < 5 - 7
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+print "Oh, that's why it's False."
+print "How about some more."
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+
+
+
+
+
+
